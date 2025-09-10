84c7b8d8d45634df78ad60178497aa9c753aaed2
TheCatFather4 <marcelloja.csharp@gmail.com>
2025-09-10 17:15:55 -0400

    Updated shopping cart
    Updated views for shopping cart.

---
+++ Portfolio/Portfolio/Controllers/ShoppingCartController.cs
@@ -0,0 +1,115 @@
+﻿using Cafe.Core.Interfaces.Services;
+using Cafe.Core.Interfaces.Services.MVC;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
+using Portfolio.Models.Ordering;
+
+namespace Portfolio.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IShoppingBagService _shoppingBagService;
+        private readonly IMVCustomerService _customerService;
+
+        public ShoppingCartController(UserManager<IdentityUser> userManager, IShoppingBagService shoppingBagService, IMVCustomerService customerService)
+        {
+            _userManager = userManager;
+            _shoppingBagService = shoppingBagService;
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var model = new ShoppingCart();
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var customerResult = await _customerService.GetCustomerByEmailAsync(User.Identity.Name);
+
+                if (customerResult.Ok)
+                {
+                    var cartResult = await _shoppingBagService.GetShoppingBagAsync(customerResult.Data.CustomerID);
+
+                    if (cartResult.Ok)
+                    {
+                        model.CustomerID = customerResult.Data.CustomerID;
+                        model.ShoppingBagID = cartResult.Data.ShoppingBagID;
+                        model.Items = cartResult.Data.Items;
+
+                        return View(model);
+                    }
+
+                    TempData["Alert"] = Alert.CreateError("An error occurred. Please contact the administrator.");
+                    return RedirectToAction("Index", "Cafe");
+                }
+
+                TempData["Alert"] = Alert.CreateError("An error occurred. Please contact the administrator.");
+                return RedirectToAction("Index", "Cafe");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddItem(int itemId)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var result = await _shoppingBagService.GetItemWithPriceAsync(itemId);
+
+                if (result.Ok)
+                {
+                    var model = new CartItem()
+                    {
+                        ItemID = itemId,
+                        ItemName = result.Data.ItemName,
+                        Quantity = 0,
+                        Price = result.Data.Prices.FirstOrDefault(p => p.ItemID == itemId).Price,
+                    };
+
+                    return View(model);
+                }
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddItem(CartItem model)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (model.Quantity <= 0)
+                {
+                    TempData["Alert"] = Alert.CreateError("Quantity must be at least 1.");
+                    return View(model);
+                }
+
+                var customerResult = await _customerService.GetCustomerByEmailAsync(User.Identity.Name);
+
+                if (customerResult.Ok)
+                {
+                    var result = await _shoppingBagService.MVCAddItemToBagAsync((int)customerResult.Data.ShoppingBagID, model.ItemID, model.ItemName, (decimal)model.Price, model.Quantity);
+
+                    if (result.Ok)
+                    {
+                        TempData["Alert"] = Alert.CreateSuccess(result.Message);
+                        return RedirectToAction("Index", "ShoppingCart");
+                    }
+
+                    TempData["Alert"] = Alert.CreateError("An error ocurred.");
+                    return RedirectToAction("Index", "Cafe");
+                }
+
+                TempData["Alert"] = Alert.CreateError("An error ocurred.");
+                return RedirectToAction("Index", "ShoppingCart");
+            }
+
+            return View();
+        }
+    }
+}
