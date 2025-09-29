d43a71f9f77073e8e784478438f4ee26efdce17c
TheCatFather4 <marcelloja.csharp@gmail.com>
2025-09-29 17:27:36 -0400

    Added validation to Item Form
    The ItemForm model now has validation that checks the prices.
    
    The style was also updated for the asoociated view for consistency.

--- Portfolio/Portfolio/Models/ItemForm.cs
+++ Portfolio/Portfolio/Models/ItemForm.cs
@@ -1,13 +1,19 @@
 ﻿using Cafe.Core.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace Portfolio.Models
 {
-    public class ItemForm
+    public class ItemForm : IValidatableObject
     {
         public int? ItemID { get; set; }
         public int? CategoryID { get; set; }
+
+        [Required(ErrorMessage = "An item name is required.")]
         public string? ItemName { get; set; }
+
+        [Required(ErrorMessage = "A description for the item is required.")]
         public string? ItemDescription { get; set; }
+
         public List<ItemPrice>? Prices { get; set; }
 
         public ItemForm()
@@ -34,6 +40,25 @@ namespace Portfolio.Models
                 ItemDescription = ItemDescription,
                 Prices = Prices
             };
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var errors = new List<ValidationResult>();
+
+            for (int i = 0; i < Prices.Count; i++)
+            {
+                var itemPrice = Prices[i];
+
+                if (itemPrice.Price.HasValue && (itemPrice.Price < 1 || itemPrice.Price > 20))
+                {
+                    string memberName = $"{nameof(Prices)}[{i}].{nameof(ItemPrice.Price)}";
+
+                    errors.Add(new ValidationResult("Price must be atleast 1 dollar and less than 20 dollars.", new[] { memberName }));
+                }
+            }
+
+            return errors;
         }
     }
 }
