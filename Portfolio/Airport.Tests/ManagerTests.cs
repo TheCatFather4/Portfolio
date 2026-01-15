14cfd79fbfb727e604dd0161a3b20812cdec5189
TheCatFather4 <marcelloja.csharp@gmail.com>
2026-01-15 14:37:42 -0500

    Unit tests added for Airport.Core
    The Airport.Core project is now unit tested.

---
+++ Portfolio/Airport.Tests/ManagerTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using Portfolio.Models.Airport;
+
+namespace Airport.Tests
+{
+    [TestFixture]
+    public class ManagerTests
+    {
+        public LockerManager GetLockerManager()
+        {
+            return new LockerManager(new MockLockerRepository());
+        }
+
+        [Test]
+        public void GetAllLockers_Success()
+        {
+            var mgr = GetLockerManager();
+            var result = mgr.GetAllLockers();
+
+            Assert.That(result.Ok, Is.True);
+            Assert.That(result.Data?.Count(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GetLocker_Success()
+        {
+            var mgr = GetLockerManager();
+            var result = mgr.GetLocker(1);
+
+            Assert.That(result.Ok, Is.True);
+            Assert.That(result.Data?.RenterName, Is.EqualTo("Test"));
+        }
+
+        [Test]
+        public void GetLocker_Fail()
+        {
+            var mgr = GetLockerManager();
+            var result = mgr.GetLocker(2);
+
+            Assert.That(result.Ok, Is.False);
+        }
+
+        [Test]
+        public void IsRented_Yes()
+        {
+            var mgr = GetLockerManager();
+            var tf = mgr.IsRented(1);
+
+            Assert.That(tf, Is.True);
+        }
+
+        [Test]
+        public void IsRented_No()
+        {
+            var mgr = GetLockerManager();
+            var tf = mgr.IsRented(2);
+
+            Assert.That(tf, Is.False);
+        }
+    }
+}
---
+++ Portfolio/Airport.Tests/MockLockerRepository.cs
@@ -0,0 +1,58 @@
+﻿using Airport.Core.DTOs;
+using Airport.Core.Interfaces;
+
+namespace Airport.Tests
+{
+    public class MockLockerRepository : ILockerRepository
+    {
+        public string? EndLockerRental(int lockerNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Locker[] GetAllLockers()
+        {
+            var lockers = new Locker[2];
+
+            lockers[0] = new Locker
+            {
+                RenterName = "A",
+                Contents = "Food"
+            };
+
+            lockers[1] = new Locker();
+
+            return lockers;
+        }
+
+        public Locker GetLocker(int lockerNumber)
+        {
+            if (lockerNumber == 1)
+            {
+                return new Locker
+                {
+                    LockerNumber = lockerNumber,
+                    RenterName = "Test",
+                    Contents = "Food"
+                };
+            }
+
+            return new Locker();
+        }
+
+        public bool IsRented(int lockerNumber)
+        {
+            if (lockerNumber == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void RentLocker(Locker locker)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
