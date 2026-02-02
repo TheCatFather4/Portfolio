9250a87057cd36c91de4aa762da6581dcbf0f2f3
TheCatFather4 <marcelloja.csharp@gmail.com>
2026-02-02 13:19:21 -0500

    Using statements and docs
    I updated a few using statements on the views concerning ordering and the shopping cart.
    I also added code documentation to the customer attribute class for decimal range.

--- Portfolio/Cafe.Core/Attributes/DecimalRange.cs
+++ Portfolio/Cafe.Core/Attributes/DecimalRange.cs
@@ -11,6 +11,12 @@ namespace Cafe.Core.Attributes
         private readonly decimal _min;
         private readonly decimal _max;
 
+        /// <summary>
+        /// Constructs the object required to validate a range of decimal values.
+        /// </summary>
+        /// <param name="min">The minimum decimal.</param>
+        /// <param name="max">The maximum decimal.</param>
+        /// <exception cref="ArgumentException">An exception will be thrown if the strings are not parsable.</exception>
         public DecimalRange(string min, string max)
         {
             if (!decimal.TryParse(min, out _min) || !decimal.TryParse(max, out _max))
@@ -19,7 +25,13 @@ namespace Cafe.Core.Attributes
             }
         }
 
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        /// <summary>
+        /// An override for the IsValid method. Checks if range is valid.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="validationContext"></param>
+        /// <returns>Success or an error message depending on the input.</returns>
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
             if (value == null)
             {
