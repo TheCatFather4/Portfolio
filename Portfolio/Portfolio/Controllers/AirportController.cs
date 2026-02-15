e04fa857c08f15903ffe38d9052aa65c6e82aab9
TheCatFather4 <marcelloja.csharp@gmail.com>
2026-02-15 15:57:28 -0500

    Code Documentation for AirportController.cs
    I added code documentation for the entire controller. I removed an unnecessary field as well.

--- Portfolio/Portfolio/Controllers/AirportController.cs
+++ Portfolio/Portfolio/Controllers/AirportController.cs
@@ -4,23 +4,37 @@ using Portfolio.Models.Airport;
 
 namespace Portfolio.Controllers
 {
+    /// <summary>
+    /// Handles HTTP requests concerning the web version of the Airport Locker Rental application.
+    /// </summary>
     public class AirportController : Controller
     {
         private readonly LockerManager _lockerManager;
-        public int lockerNumber;
 
+        /// <summary>
+        /// Constructs a controller with the necessary member in order to access lockers.
+        /// </summary>
+        /// <param name="lockerManager"></param>
         public AirportController(LockerManager lockerManager)
         {
             _lockerManager = lockerManager;
-            lockerNumber = 0;
         }
 
+        /// <summary>
+        /// Takes the user to the main menu page.
+        /// </summary>
+        /// <returns>A ViewResult object with the model state.</returns>
         public IActionResult Menu()
         {
             var model = new AirportMenu();
             return View(model);
         }
 
+        /// <summary>
+        /// Validates the user input and switches their choice to the appropriate controller.
+        /// </summary>
+        /// <param name="model">Used for menu input and display</param>
+        /// <returns>A RedirectToActionResult object.</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Menu(AirportMenu model)
@@ -46,12 +60,23 @@ namespace Portfolio.Controllers
             return View(model);
         }
 
+        /// <summary>
+        /// Takes the user to a page where they are "prompted" to select a locker to view.
+        /// (Menu Choice 1: Input)
+        /// </summary>
+        /// <returns>A ViewResult object with the model state.</returns>
         public IActionResult ViewLocker()
         {
             var model = new ViewLockerForm();
             return View(model);
         }
 
+        /// <summary>
+        /// Validates the user input and retrieves the appropriate locker object.
+        /// (Menu Choice 1: Validation)
+        /// </summary>
+        /// <param name="model">Used to retrieve locker choice and display locker data.</param>
+        /// <returns>A ViewResult object with the model state.</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult ViewLocker(ViewLockerForm model)
@@ -74,12 +99,23 @@ namespace Portfolio.Controllers
             return View(model);
         }
 
+        /// <summary>
+        /// Takes the user to a web page where they can select a locker number for rental.
+        /// (Menu Choice 2.1: Input)
+        /// </summary>
+        /// <returns>A ViewResult object with the model state.</returns>
         public IActionResult RentLocker()
         {
             var model = new RentLockerForm();
             return View(model);
         }
 
+        /// <summary>
+        /// Validates the user's input. If successful the user is redirected to a web page where they
+        /// enter their renter details. (Display for Menu Choice 2.1: Validation)
+        /// </summary>
+        /// <param name="model">Used to retrieve locker number choice and display result message.</param>
+        /// <returns>If selected locker is available, A RedirectToActionResult object is returned.</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult RentLocker(RentLockerForm model)
@@ -94,14 +130,18 @@ namespace Portfolio.Controllers
                     return View(model);
                 }
 
-                lockerNumber = choice;
-                return RedirectToAction("AddLocker", new { id = lockerNumber });
+                return RedirectToAction("AddLocker", new { id = choice });
             }
 
             model.Message = "Invalid input. Please choose a number from 1 to 100.";
             return View(model);
         }
 
+        /// <summary>
+        /// Takes the user to a web page where they can supply their renter details. (Menu Choice 2.2: Input)
+        /// </summary>
+        /// <param name="id">The selected locker number.</param>
+        /// <returns>A ViewResult object with the model state.</returns>
         public IActionResult AddLocker(int id)
         {
             var model = new AddLockerForm();
@@ -109,6 +149,11 @@ namespace Portfolio.Controllers
             return View(model);
         }
 
+        /// <summary>
+        /// Rents a locker for a user. (Menu Choice 2.2: Validation)
+        /// </summary>
+        /// <param name="model">The renter's data.</param>
+        /// <returns>A Viewresult object with the model state.</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult AddLocker(AddLockerForm model)
@@ -142,12 +187,21 @@ namespace Portfolio.Controllers
             return RedirectToAction("Menu");
         }
 
+        /// <summary>
+        /// Takes the user to a web page where they can select a locker to end their rental. (Menu Choice 3: Input)
+        /// </summary>
+        /// <returns>A Viewresult object with the model state.</returns>
         public IActionResult EndRental()
         {
             var model = new EndRentalForm();
             return View(model);
         }
 
+        /// <summary>
+        /// Validates the user's input for ending a locker rental. (Menu Choice 3: Validation)
+        /// </summary>
+        /// <param name="model">Used for user input and validation.</param>
+        /// <returns>A RedirectToActionResult.</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult EndRental(EndRentalForm model)
@@ -162,14 +216,18 @@ namespace Portfolio.Controllers
                     return View(model);
                 }
 
-                lockerNumber = choice;
-                return RedirectToAction("DeleteLocker", new { id = lockerNumber });
+                return RedirectToAction("DeleteLocker", new { id = choice });
             }
 
             model.Message = "Invalid input. Please choose a number from 1 to 100.";
             return View(model);
         }
 
+        /// <summary>
+        /// Ends the locker rental for the user. (Menu Choice 3: Action)
+        /// </summary>
+        /// <param name="id">The selected locker number to delete.</param>
+        /// <returns>A Viewresult object with the model state.</returns>
         public IActionResult DeleteLocker(int id)
         {
             var result = _lockerManager.EndLockerRental(id);
@@ -184,6 +242,10 @@ namespace Portfolio.Controllers
             return RedirectToAction("Menu");
         }
 
+        /// <summary>
+        /// Displays all rented lockers and their contents.
+        /// </summary>
+        /// <returns>A ViewResult object.</returns>
         public IActionResult AllLockers()
         {
             var model = new AllLockersDisplay();
@@ -199,6 +261,10 @@ namespace Portfolio.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Take the user to a web page where they "exit" the application.
+        /// </summary>
+        /// <returns>A ViewResult object.</returns>
         public IActionResult Exit()
         {
             return View();
