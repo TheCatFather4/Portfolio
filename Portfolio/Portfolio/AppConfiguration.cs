f5763a97b07f3e4b150f4793a3f06c25043ed8a6
TheCatFather4 <marcelloja.csharp@gmail.com>
2026-02-17 13:47:57 -0500

    Updated AppConfiguration.cs
    Added code documentation.

--- Portfolio/Portfolio/AppConfiguration.cs
+++ Portfolio/Portfolio/AppConfiguration.cs
@@ -3,20 +3,36 @@ using Cafe.Core.Interfaces.Application;
 
 namespace Portfolio
 {
+    /// <summary>
+    /// Implements IAppConfiguration to handle the configuration of the 4th Wall Café application.
+    /// </summary>
     public class AppConfiguration : IAppConfiguration
     {
         private readonly IConfiguration _configuration;
 
+        /// <summary>
+        /// Constructs a configuration object with the required dependency.
+        /// </summary>
+        /// <param name="configuration">An implementation of the IConfiguration interface.</param>
         public AppConfiguration(IConfiguration configuration)
         {
             _configuration = configuration;
         }
 
+        /// <summary>
+        /// Retrieves the connection string from the appsettings.json file.
+        /// </summary>
+        /// <returns>A connection string</returns>
         public string GetConnectionString()
         {
             return _configuration.GetConnectionString("CafeDb") ?? "";
         }
 
+        /// <summary>
+        /// Retrieves the database mode from the appsettings.json file.
+        /// </summary>
+        /// <returns>A DatabaseMode enum.</returns>
+        /// <exception cref="Exception"></exception>
         public DatabaseMode GetDatabaseMode()
         {
             if (string.IsNullOrEmpty(_configuration["DatabaseMode"]))
