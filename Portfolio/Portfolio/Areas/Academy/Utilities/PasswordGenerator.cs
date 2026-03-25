72fe7a131b22069e1f1f4100d3fac160d27c675f
TheCatFather4 <marcelloja.csharp@gmail.com>
2026-03-25 16:24:08 -0400

    Created PasswordGenerator.cs

---
+++ Portfolio/Portfolio/Areas/Academy/Utilities/PasswordGenerator.cs
@@ -0,0 +1,39 @@
+﻿namespace Portfolio.Areas.Academy.Utilities
+{
+    public class PasswordGenerator
+    {
+        public static string GeneratePassword()
+        {
+            int length = 0;
+            string letters = "qQwWeErRtTyYuUiIoOpPaAsSdDfFgGhHjJkKlLzZxXcCvVbBnNmM";
+            string numbers = "1234567890!@#$%^&*?.";
+            Random lengthGen = new Random();
+            Random rlg = new Random();
+            Random rng = new Random();
+            Random charType = new Random();
+
+            length = lengthGen.Next(8, 13);
+
+            char[] parts = new char[length];
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                int ct = charType.Next(1, 3);
+
+                if (ct == 1)
+                {
+                    int le = rlg.Next(0, 52);
+                    parts[i] = letters[le];
+                }
+                else
+                {
+                    int ne = rng.Next(0, 20);
+                    parts[i] = numbers[ne];
+                }
+            }
+
+            string password = new string(parts);
+            return password;
+        }
+    }
+}
