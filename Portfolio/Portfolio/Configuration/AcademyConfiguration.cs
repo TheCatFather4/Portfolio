d5ce419556069f37025db25a008746eb080e4ce0
TheCatFather4 <marcelloja.csharp@gmail.com>
2026-03-26 11:52:15 -0400

    Created Academy Layout and Configuration

---
+++ Portfolio/Portfolio/Configuration/AcademyConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Academy.Core.Interfaces.Application;
+
+namespace Portfolio.Configuration
+{
+    /// <summary>
+    /// Implements IAcademyConfiguration to handle the configuration of the 4th Wall Academy application.
+    /// </summary>
+    public class AcademyConfiguration : IAcademyConfiguration
+    {
+        private readonly IConfiguration _configuration;
+
+        /// <summary>
+        /// Constructs a configuration object with the required dependency.
+        /// </summary>
+        /// <param name="configuration">An implementation of the IConfiguration interface.</param>
+        public AcademyConfiguration(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Retrieves the connection string from the appsettings.json file.
+        /// </summary>
+        /// <returns>A connection string</returns>
+        public string GetConnectionString()
+        {
+            return _configuration.GetConnectionString("CafeDb") ?? "";
+        }
+    }
+}
